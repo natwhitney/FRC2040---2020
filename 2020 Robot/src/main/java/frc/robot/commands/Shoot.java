7c946564e7d13d1905503f2812f7582419445340
Frantic9 <49459110+Frantic9@users.noreply.github.com>
2020-01-11 15:47:11 -0600

    Adds Shooter Subsystems and shoot command

---
+++ 2020 Robot/src/main/java/frc/robot/commands/Shoot.java
@@ -0,0 +1,17 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Shoot extends CommandBase {
+    private final Shooter m_Shooter;
+
+    public Shoot(Shooter subsystem){
+        m_Shooter = subsystem;
+        addRequirements(m_Shooter);
+    }
+
+    public void execute(){
+        
+    }
+}
